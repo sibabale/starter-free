95c5f0536fdabc5d69864a602f60b1d116924ca0
Sibabale Joja <jojasibabale@gmail.com>
2024-12-09 05:38:53 +0200

    refactor: updated metadata

--- apps/next/app/layout.tsx
+++ apps/next/app/layout.tsx
@@ -2,18 +2,15 @@ import type { Metadata } from 'next'
 import { NextTamaguiProvider } from './NextTamaguiProvider'
 
 export const metadata: Metadata = {
-  title: 'Tamagui • App Router',
-  description: 'Tamagui, Solito, Expo & Next.js',
+  title: 'Nota',
+  description: 'A simple note-taking app',
   icons: '/favicon.ico',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     // You can use `suppressHydrationWarning` to avoid the warning about mismatched content during hydration in dev mode
-    <html
-      lang="en"
-      suppressHydrationWarning
-    >
+    <html lang="en" suppressHydrationWarning>
       <body>
         <NextTamaguiProvider>{children}</NextTamaguiProvider>
       </body>
